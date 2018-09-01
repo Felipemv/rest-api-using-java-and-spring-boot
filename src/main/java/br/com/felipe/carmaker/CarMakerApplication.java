632132c698191580d1daea96ad7fc09a0767f980
Felipemv <felipe.martinsvitor@gmail.com>
2018-08-31 23:38:00 -0300

    Basic structure of the project.

---
+++ src/main/java/br/com/felipe/carmaker/CarMakerApplication.java
@@ -0,0 +1,12 @@
+package br.com.felipe.carmaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarMakerApplication.class, args);
+	}
+}
